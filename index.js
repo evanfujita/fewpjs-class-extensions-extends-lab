59432bddd84b2d01c5eda8a8411c0736e35c2286
Evan <evanfujita@gmail.com>
2021-02-22 20:51:05 -0600

    commit tree 255215c81bc2c5f71b82cd7c54e399508e3068c4

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+class Polygon {
+
+    constructor(sides){
+        this.sides = sides
+    }
+
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter(){
+        this.sides.reduce((total, num) =>  total + num
+        )
+    }
+    
+
+}
+
+class Triangle extends Polygon {
+
+    
+}
